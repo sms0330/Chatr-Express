510e0373e16d1b5bd0f594277d1d93587b60260f
Joseph Son <sms0330@gmail.com>
2021-08-03 19:54:12 -0700

    Fixed route

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {index, create, update, destroy, changeFlag} = require('../controllers/messages');
+const {index, create, update, destroy} = require('../controllers/messages');
 
 router.get('/', index);
 router.post('/', create);
